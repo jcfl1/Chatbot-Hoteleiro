c0731a300fcb7fcdccd51f03fa65880c7fe69f44
Bianca Carneiro <bianca.ccnf@gmail.com>
2024-07-17 08:35:45 -0300

    Mudando a pasta e mensagem de /help e /start

--- src/main.py
+++ src/main.py
@@ -19,20 +19,14 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TELEGRAM_TOKEN).build()
-
-    # on different commands - answer in Telegram
+    
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
+    
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
--- src/telegram_api.py
+++ src/telegram_api.py
@@ -20,33 +20,22 @@ import os
 from dotenv import load_dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-
-TOKEN = os.getenv('TELEGRAM_TOKEN')
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
+from rag import RAGApplication
 
 
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        rf"Oi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
 
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
+    await update.message.reply_text(f"""Você pode perguntar qualquer coisa sobre Hotelaria em Porto de Galinhas que eu saberei responder 😁.""")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
+
