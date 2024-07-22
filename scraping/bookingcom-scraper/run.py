8e01f5221b6d9412fe77faebcced92dc1fba03ff
Joao Felipe Sobral <jfmvs@hotmail.com>
2024-07-22 10:18:53 -0300

    Scraping

---
+++ scraping/bookingcom-scraper/run.py
@@ -0,0 +1,42 @@
+"""
+This example run script shows how to run the bookingcom scraper defined in ./bookingcom.py
+It scrapes search and hotel data and saves it to ./results/
+
+To run this script set the env variable $SCRAPFLY_KEY with your scrapfly API key:
+$ export $SCRAPFLY_KEY="your key from https://scrapfly.io/dashboard"
+"""
+import asyncio
+from datetime import datetime, timedelta
+import json
+from pathlib import Path
+import bookingcom
+
+output = Path(__file__).parent / "results"
+output.mkdir(exist_ok=True)
+
+
+TODAY = datetime.now().strftime('%Y-%m-%d')
+WEEK_FROM_NOW = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d')
+MONTH_FROM_NOW = (datetime.now() + timedelta(days=30)).strftime('%Y-%m-%d')
+
+async def run():
+    print("running Booking.com example scrapes and saving results to ./results directory")
+
+    # result_search = await bookingcom.scrape_search(
+    #     query="Malta",
+    #     checkin=TODAY,
+    #     checkout=WEEK_FROM_NOW,
+    #     max_pages=2
+    # )
+    # output.joinpath("search.json").write_text(json.dumps(result_search, indent=2, ensure_ascii=False), encoding="utf-8")
+
+    result_hotel = await bookingcom.scrape_hotel(
+        "https://www.booking.com/hotel/br/pousada-porto-de-galinhas.pt-br.html",
+        checkin=WEEK_FROM_NOW, 
+        price_n_days=7,
+    )
+    output.joinpath("hotel.json").write_text(json.dumps(result_hotel, indent=2, ensure_ascii=False), encoding="utf-8")
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
---
+++ scraping/bookingcom-scraper/test.py
@@ -0,0 +1,65 @@
+"""
+"""
+from datetime import datetime, timedelta
+import pytest
+from cerberus import Validator
+import bookingcom
+
+# enable cache?
+# bookingcom.BASE_CONFIG["cache"] = True
+NOW = datetime.now().strftime('%Y-%m-%d')
+WEEK_FROM_NOW = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d')
+MONTH_FROM_NOW = (datetime.now() + timedelta(days=30)).strftime('%Y-%m-%d')
+bookingcom.BASE_CONFIG["debug"] = True
+
+
+@pytest.mark.asyncio
+async def test_search_scraping():
+    result_search = await bookingcom.scrape_search(query="Malta", checkin="2023-06-10", checkout="2023-06-20", max_pages=3)
+    assert len(result_search) >= 50
+
+
+@pytest.mark.asyncio
+async def test_hotel_scraping():
+    bookingcom.BASE_CONFIG["cache"] = False
+    urls = [
+        "https://www.booking.com/hotel/gb/gardencourthotel.en-gb.html",
+        "https://www.booking.com/hotel/fr/hotelf1-paris-porte-de-montmartre.en-gb.html",
+    ]
+    for url in urls:
+        item = await bookingcom.scrape_hotel(
+            url,
+            checkin=NOW,
+        )
+        assert item
+        schema = {
+            "url": {"type": "string"},
+            "title": {"type": "string", "minlength": 4},
+            "description": {"type": "string"},
+            "address": {"type": "string"},
+            "images": {"type": "list"},
+            "lat": {"type": "float", "coerce": float},
+            "lng": {"type": "float", "coerce": float},
+            'price': {
+                'type': 'list',
+                'schema': {
+                    'type': 'dict',
+                    'schema': {
+                        'available': {'type': 'boolean'},
+                        'checkin': {'type': 'string', 'regex': r"[0-9]{4}-[0-9]{2}-[0-9]{2}"},
+                        'minLengthOfStay': {'type': 'integer'},
+                        'avgPriceFormatted': {'type': 'string'},
+                    }
+                }
+            },
+            'features': {
+                'type': 'dict',
+                'valuesrules': {
+                    'type': 'list',
+                    'schema': {'type': 'string'},
+                }
+            },
+        }
+        validator = Validator(schema, allow_unknown=True)
+        if not validator.validate(item):
+            raise Exception({"item": item, "errors": validator.errors})
